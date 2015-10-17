2492f06d7728b3213f760930336dff9a9d1188c1
David Jones <unixmonkey1@gmail.com>
2015-10-17 08:23:11 -0400

    Make ball roll

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	// Updated before physics calculations
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement);
 	}
 }
