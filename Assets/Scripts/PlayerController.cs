49d9cc12105499136111949dfbb141337c926af1
David Jones <unixmonkey1@gmail.com>
2015-10-18 18:16:29 -0400

    Display a winning message on all pickups collected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
 	private int count;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour {
 		rb = GetComponent<Rigidbody>();
 		count = 0;
 		SetCountText();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
@@ -40,6 +42,9 @@ public class PlayerController : MonoBehaviour {
 
 	void SetCountText() {
 		countText.text = "Count: " + count.ToString();
+		if (count >= 4) {
+			winText.text = "YOU WIN!!!";
+		}
 	}
 
 }
